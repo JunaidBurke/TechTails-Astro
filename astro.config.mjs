12d2c56e41426aa337beacc4a4eb1525afd4b560
Junaid Burke <46665119+JunaidBurke@users.noreply.github.com>
2023-11-26 21:42:16 -0500

    Add @astrojs/node dependency and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://junaidburke.com',
-  integrations: [tailwind(), sitemap(), react()]
+  integrations: [tailwind(), sitemap(), react()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone"
+  })
 });
