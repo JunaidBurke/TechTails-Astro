c791b4f0877519b5f1da5dd212a1177a6f192001
Junaid Burke <46665119+JunaidBurke@users.noreply.github.com>
2023-11-30 18:58:53 -0500

    Add database migration and configuration files

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'drizzle-kit'
+
+export default {
+
+    schema: './src/db/index.ts',
+    out: './src/db/migrations',
+    driver: 'better-sqlite',
+    verbose: true,
+
+    dbCredentials: {
+        url: process.env.DB_PATH || './local.db',
+    }
+
+} satisfies Config
---
+++ src/db/index.ts
@@ -0,0 +1,16 @@
+import Database from 'better-sqlite3'
+
+import {
+     drizzle, 
+     type BetterSQLite3Database, 
+} from 'drizzle-orm/better-sqlite3'
+
+import { migrate } from 'drizzle-orm/better-sqlite3/migrator'
+
+const sqlite = new Database(process.env.DB_PATH || './local.db')
+
+
+export const db: BetterSQLite3Database = drizzle(sqlite, {logger: true})
+migrate(db, { migrationsFolder: './src/db/migrations'})
+
+export { likes } from './schema'
